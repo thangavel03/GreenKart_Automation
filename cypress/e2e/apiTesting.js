5bef7437d1a4ea49701cb7a936ec789aba7c83ec
Thangavel <thangavelra2003@gmail.com>
2025-03-14 12:07:43 +0530

    update

--- cypress/e2e/apiTesting.js
+++
@@ -1,8 +0,0 @@
-describe('API Testing with Cypress', () => {
-    it('GET - Fetch Users', () => {
-      cy.request('GET', 'https://jsonplaceholder.typicode.com/users').then((response) => {
-        expect(response.status).to.eq(200);
-        expect(response.body).to.have.length(10);
-      });
-    });
-  });
